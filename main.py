c06982973771e816ab9e71af61d18b88886b2851
i'm <tushminskyi42@gmail.com>
2023-11-27 19:52:19 +0700

    new place

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-n = int(input())
-n = n % (24*60)
-h = n // 60
-m = n % 60
-print(h,m)
+a=None
+max1=max2=0
+
+while a!=0:
+    a = int(input())
+    if a > max1:
+        max2 = max1
+        max1 = a
+
+    else:
+        if a < max1 and a!=0:
+            max2 = a
+            print(max2)
+
+
+
+print(max2)
